91b3883e044d6b7a9ed9ce012851e1629e0cd5d8
luwenjing <1054748993@qq.com>
2018-10-14 23:41:58 +0800

    ceshi

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,14 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('index',"IndexController@index");
+Route::get('liebiao',"IndexController@liebiao");
+Route::get('details',"IndexController@details");
+Route::get('shopcart',"Xiaomi/IndexController@shopcart");
+Route::get('login',"Xiaomi/UserController@login");
+Route::get('register',"Xiaomi/UserController@register");
+Route::get('self_info',"Xiaomi/UserController@self_info");
+Route::get('order',"Xiaomi/UserController@order");
+
+
+
